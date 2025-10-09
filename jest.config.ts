7552acb63bef7ddf674e9650546cafd93dd39d6c
stephane <73640160+stef38-code@users.noreply.github.com>
2025-10-09 21:07:02 +0200

    feature/init_app (#1)
    * **feat(feature/init_app): Simplification de la config Jest et ajustement des scripts npm**
    
    - Suppression de l'ancien contenu et nettoyage de `jest.config.ts` pour une configuration plus concise et adaptée.
    - Ajout de `pathsToModuleNameMapper` pour la gestion des alias TypeScript à partir de `tsconfig.json`.
    - Mise à jour du script npm `test` dans `package.json` en ajoutant l'option `--no-cache`.
    - Révision de la configuration pour garantir le support des chemins de module TypeScript et des modules ES modernes.

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+const tsconfig = require('./tsconfig.json');
+
+const config: Config = {
+  moduleNameMapper: pathsToModuleNameMapper(
+    tsconfig.compilerOptions.paths || {},
+    {
+      prefix: '<rootDir>/',
+    },
+  ),
+};
+
+export default config;
