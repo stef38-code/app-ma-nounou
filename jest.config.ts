2865df1eeb20e47fdfb0bcc00eaba26de571442b
stephane <stephane.betton@gmail.com>
2025-10-12 18:34:45 +0200

    feat(feature/init_app): ajout d'une exclusion pour les tests E2E dans Jest
    - Mise à jour du fichier `jest.config.ts` :
      - Ajout du dossier `/e2e` dans la liste des `testPathIgnorePatterns`.
    - Prévention de l'exécution des tests E2E dans les suites Jest.

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from 'jest';
 import { pathsToModuleNameMapper } from 'ts-jest';
+
 const tsconfig = require('./tsconfig.json');
 
 const config: Config = {
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/e2e'],
   moduleNameMapper: pathsToModuleNameMapper(
     tsconfig.compilerOptions.paths || {},
     {
